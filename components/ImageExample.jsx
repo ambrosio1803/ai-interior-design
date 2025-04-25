b0e49c52492e11635a55590a0effbddd1645bf7f
ambrosio1803 <ambrosio02.inv@gmail.com>
2025-04-25 02:22:10 +0200

    Delete components directory

--- components/ImageExample.jsx
+++
@@ -1,8 +0,0 @@
-export default function ImageExample({ title, src }) {
-  return (
-    <div>
-      <img src={src} alt={title} className="rounded shadow" />
-      <p className="mt-2 text-center">{title}</p>
-    </div>
-  )
-}
