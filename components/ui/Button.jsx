b10b6e05429925fce22de3472d2dee49bcba3878
ambrosio1803 <ambrosio02.inv@gmail.com>
2025-04-25 02:33:54 +0200

    Delete components directory

--- components/ui/Button.jsx
+++
@@ -1,7 +0,0 @@
-export default function Button({ children, onClick }) {
-  return (
-    <button onClick={onClick} className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      {children}
-    </button>
-  )
-}
